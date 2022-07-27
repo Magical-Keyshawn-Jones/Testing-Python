f9f953e8bd512211829194299673dfe1fffd9ed1
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-07-27 13:36:02 -0500

    Added freeCodeAcademy Course folder into this

---
+++ numbers.py
@@ -0,0 +1,11 @@
+#  + Addition
+#  - Subtraction
+#  * Multiplication
+#  / Division (Float)
+#  // Floor Division (Integer)
+#  % Modulus (Remainder)
+#  ** Exponents (5**3 === 5*5*5)
+
+# Built In Features
+
+# round() - round(number, decimal places)
